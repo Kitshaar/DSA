ef3af329d958abd2563ec010eae4eedbba7100a1
Kartik Sharma <sharmakartik2322002@gmail.com>
2025-01-18 12:25:17 +0530

    Practice stack implementation using Link list and Arrays.

--- dsa_practice/src/main/java/org/example/App.java
+++ dsa_practice/src/main/java/org/example/App.java
@@ -1,13 +1,32 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        FixedCapacityStackOfStrings fixedStack = new FixedCapacityStackOfStrings();
+        StackOfStrings stack = new StackOfStrings();
+        // Creating a test case for the StackOfStings Linked list implementation
+        // Chose to use Collection api to store values instead to getting values from the console or
+        // a file
+        List<String> todo = Arrays.asList("to ", "be ", "or ", "not ", "to ",
+                "- ", "be ", "- ", "- ", "that ", "- ", "- ", "- ", "is ");
+        for (String s : todo)
+        {
+            if (s.equals("- "))
+                System.out.print(stack.pop());
+            else
+                stack.push(s);
+        }
+
     }
 }
--- dsa_practice/src/main/java/org/example/FixedCapacityStackOfStrings.java
+++ dsa_practice/src/main/java/org/example/FixedCapacityStackOfStrings.java
@@ -1,4 +1,30 @@
 package org.example;
 
 public class FixedCapacityStackOfStrings {
+    private String [] s;
+    private Integer n = 0;
+
+    public FixedCapacityStackOfStrings() {}
+    public FixedCapacityStackOfStrings(int capacity)
+    {
+        s = new String[capacity];
+    }
+
+    public boolean isEmpty()
+    {
+        return n == 0;
+    }
+
+    public void push(String value)
+    {
+        s[n++] = value;
+    }
+
+    public String pop()
+    {
+        String item = s[--n];
+        s[n] = null;
+        return item;
+    }
+
 }
